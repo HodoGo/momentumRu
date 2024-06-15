5c522a9777e204391911ef4d416c561b39c47154
AhmadIkbalDjaya <ikbaldjaya@gmail.com>
2024-06-15 21:43:13 +0800

    update: student_quiz_work mount student answer from db

--- app/Livewire/User/Quiz/Work.php
+++ app/Livewire/User/Quiz/Work.php
@@ -4,6 +4,7 @@ namespace App\Livewire\User\Quiz;
 
 use App\Models\Question;
 use App\Models\Quiz;
+use App\Models\StudentQuiz;
 use Carbon\Carbon;
 use Livewire\Attributes\Layout;
 use Livewire\Component;
@@ -12,14 +13,34 @@ class Work extends Component
 {
     #[Layout("components.layouts.base_layout")]
     public Quiz $quiz;
+    public StudentQuiz $student_quiz;
     public $active_question = 1;
+    public $selected_options = [];
     // public $remaining_time;
     public function mount()
     {
+        $this->student_quiz = StudentQuiz::firstOrCreate(
+            ["student_id" => auth()->guard("student")->user()->id, "quiz_id" => $this->quiz->id],
+            ["start_time" => Carbon::now()],
+        );
+        foreach ($this->quiz->questions as $index => $question) {
+            $has_answer = false;
+            foreach ($this->student_quiz->student_quiz_answers as $student_quiz_answer) {
+                if ($question->id == $student_quiz_answer->question_id) {
+                    $this->selected_options[$index] = $student_quiz_answer->option_id;
+                    $has_answer = true;
+                    break;
+                }
+            }
+            if (!$has_answer) {
+                $this->selected_options[$index] = null;
+            }
+        }
         // $this->remaining_time = $this->count_remaining_time();
     }
     public function render()
     {
+        // dd($this->selected_options);
         $show_question = Question::where("quiz_id", $this->quiz->id)
             ->skip($this->active_question - 1)
             ->take(1)
--- resources/views/livewire/user/quiz/work.blade.php
+++ resources/views/livewire/user/quiz/work.blade.php
@@ -39,15 +39,53 @@
     <div class="flex flex-wrap md:flex-nowrap justify-between gap-x-5 gap-y-3 mt-5">
       <div class="basis-full md:basis-8/12 bg-white shadow-sm rounded-lg p-6">
         <h6 class="font-medium text-base">Nomor {{ $active_question }}</h6>
+        {{-- @foreach ($quiz->questions as $question)
+          @if ($loop->iteration == $active_question)
+            <div>
+              <div>
+                {{ $question->question }}
+              </div>
+              <div class="my-2">
+                <form action="">
+                  @foreach ($question->options as $option)
+                    <div class="flex items-start gap-1 py-3">
+                      <input type="radio" name="" id="" class="mt-2">
+                      <label for="" class="flex">
+                        <p class="me-2">
+                          @if ($loop->iteration == 1)
+                            A
+                          @elseif($loop->iteration == 2)
+                            B
+                          @elseif($loop->iteration == 3)
+                            C
+                          @elseif($loop->iteration == 4)
+                            D
+                          @endif
+                          .
+                        </p>
+                        <div>
+                          {{ $option->option }}
+                        </div>
+                      </label>
+                    </div>
+                  @endforeach
+                </form>
+              </div>
+            </div>
+          @endif
+        @endforeach --}}
         <div>
-          {{ $show_question->question }}
-        </div>
-        <div class="my-2">
-          <form action="">
+          <div>
+            {{ $show_question->question }}
+          </div>
+          <div class="my-2">
+            {{-- <form action=""> --}}
             @foreach ($show_question->options as $option)
               <div class="flex items-start gap-1 py-3">
-                <input type="radio" name="" id="" class="mt-2">
-                <label for="" class="flex">
+                <input type="radio" wire:model="selected_options.{{ $active_question - 1 }}"
+                  name="question{{ $show_question->id }}options" value="{{ $option->id }}"
+                  id="selected_options{{ $option->id }}" class="mt-2">
+                <label for="selected_options{{ $option->id }}" class="flex">
                   <p class="me-2">
                     @if ($loop->iteration == 1)
                       A
@@ -66,7 +104,8 @@
                 </label>
               </div>
             @endforeach
-          </form>
+            {{-- </form> --}}
+          </div>
         </div>
         <div class="flex {{ $active_question > 1 ? 'justify-between' : 'justify-end' }} mt-20">
           @if ($active_question > 1)
@@ -88,19 +127,32 @@
           <h6 class="px-5 py-2 bg-gray-200 rounded-t-lg font-medium">Daftar Soal</h6>
           <div class="px-6 py-6">
             <div class="grid gap-2 grid-cols-5 justify-between">
-              @foreach ($quiz->questions as $question)
+              @foreach ($quiz->questions as $index => $question)
                 <button wire:click="setQuestion({{ $loop->iteration }})"
-                  class="{{ $loop->iteration == $active_question ? 'bg-momentum1' : 'bg-gray-500' }} px-2 py-1 text-white font-medium rounded">
+                  class="{{ $loop->iteration == $active_question ? 'bg-momentum1' : ($selected_options[$index] != null ? 'bg-momentum2' : 'bg-gray-500') }} px-2 py-1 text-white font-medium rounded">
                   {{ $loop->iteration }}
                 </button>
               @endforeach
             </div>
           </div>
           <livewire:user.components.time-remaining :end_time="$quiz->end_time" />
-          {{-- <div wire:poll="refresh_time_remaining.1s" class="px-6 text-sm flex gap-2">
-            <p class="text-gray-500">Waktu Tersisa:</p>
-            <p class="font-medium">{{ $remaining_time }}</p>
-          </div> --}}
+          <div class="px-6 mt-2 flex gap-x-2">
+            <div class="flex gap-x-1 items-center">
+              <div class="h-3 w-3 bg-momentum1 rounded">
+              </div>
+              <p class="text-xs">Dilihat</p>
+            </div>
+            <div class="flex gap-x-1 items-center">
+              <div class="h-3 w-3 bg-momentum2 rounded">
+              </div>
+              <p class="text-xs">Terjawab</p>
+            </div>
+            <div class="flex gap-x-1 items-center">
+              <div class="h-3 w-3 bg-gray-500 rounded">
+              </div>
+              <p class="text-xs">Belum Dijawab</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
